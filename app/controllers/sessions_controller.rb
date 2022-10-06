5192869ea83f28414b227fbced68c1151b05dd36
JamesGabby <jlghfe36@gmail.com>
2022-10-06 16:19:10 +0100

    update flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,19 +8,20 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     # If user is valid (found) and the user authenticates
     if user && user.authenticate(params[:session][:password])
+      # In-built - creates a session for a user; Store the ID of a user in a session to keep track of the user
       session[:user_id] = user.id
       flash[:success] = "Welcome #{user.username}! You have successfully logged in."
       redirect_to root_path
     else
       # flash.now[] renders the flash immediately whereas flash[] renders it on the next render
-      flash.now[:error] = "There was something wrong with your credentials #{user.id}"
+      flash.now[:error] = "There was something wrong with your credentials."
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    flash[:success] = "You have successfully logged out."
     redirect_to login_path
   end
 end
