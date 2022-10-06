812faf68b0693fc2cb1c4ea46bc7131663d91813
JamesGabby <jlghfe36@gmail.com>
2022-10-06 15:19:51 +0100

    add authentication and flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
+  # Create a new log in (Sign up?)
   def new
+  end
+
+  # Create a log in session
+  def create
+    user = User.find_by(username: params[:session][:username])
+    # If user is valid (found) and the user authenticates
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome #{user.username}! You have successfully logged in."
+      redirect_to root_path
+    else
+      # flash.now[] renders the flash immediately whereas flash[] renders it on the next render
+      flash.now[:error] = "There was something wrong with your credentials #{user.id}"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
   end
 end
